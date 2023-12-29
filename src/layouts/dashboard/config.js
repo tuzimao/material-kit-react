4baa8772b5a5a302ac3a41e35327dda3f638dfe6
tuzimao <zliu1@ualberta.ca>
2023-12-28 19:08:05 -0800

    side-nav section name and icon change

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -1,84 +1,85 @@
-import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
-import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
-import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
-import UserIcon from '@heroicons/react/24/solid/UserIcon';
-import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
-import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
-import { SvgIcon } from '@mui/material';
+import ChatBubbleLeftIcon from "@heroicons/react/24/solid/ChatBubbleLeftIcon";
+import FolderIcon from "@heroicons/react/24/solid/FolderIcon";
+import ArrowPathIcon from "@heroicons/react/24/solid/ArrowPathIcon";
+import CogIcon from "@heroicons/react/24/solid/CogIcon";
+import LockClosedIcon from "@heroicons/react/24/solid/LockClosedIcon";
+import UserIcon from "@heroicons/react/24/solid/UserIcon";
+import UserPlusIcon from "@heroicons/react/24/solid/UserPlusIcon";
+import XCircleIcon from "@heroicons/react/24/solid/XCircleIcon";
+import { SvgIcon } from "@mui/material";
 
 export const items = [
   {
-    title: 'Overview',
-    path: '/',
+    title: "Chat",
+    path: "/",
     icon: (
       <SvgIcon fontSize="small">
-        <ChartBarIcon />
+        <ChatBubbleLeftIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Customers',
-    path: '/customers',
+    title: "History",
+    path: "/companies",
     icon: (
       <SvgIcon fontSize="small">
-        <UsersIcon />
+        <ArrowPathIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Companies',
-    path: '/companies',
+    title: "Files",
+    path: "/customers",
     icon: (
       <SvgIcon fontSize="small">
-        <ShoppingBagIcon />
+        <FolderIcon />
       </SvgIcon>
-    )
+    ),
   },
+
   {
-    title: 'Account',
-    path: '/account',
+    title: "Account",
+    path: "/account",
     icon: (
       <SvgIcon fontSize="small">
         <UserIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Settings',
-    path: '/settings',
+    title: "Settings",
+    path: "/settings",
     icon: (
       <SvgIcon fontSize="small">
         <CogIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Login',
-    path: '/auth/login',
+    title: "Login",
+    path: "/auth/login",
     icon: (
       <SvgIcon fontSize="small">
         <LockClosedIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Register',
-    path: '/auth/register',
+    title: "Register",
+    path: "/auth/register",
     icon: (
       <SvgIcon fontSize="small">
         <UserPlusIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Error',
-    path: '/404',
+    title: "Error",
+    path: "/404",
     icon: (
       <SvgIcon fontSize="small">
         <XCircleIcon />
       </SvgIcon>
-    )
-  }
+    ),
+  },
 ];
